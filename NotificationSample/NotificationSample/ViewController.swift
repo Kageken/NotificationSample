1a8d99eb03c6bb85338e7049c42be381a6ffb64e
Kageken <kageken@KagekennoMacBook-Pro.local>
2018-10-26 10:50:50 +0900

    First commit

---
+++ NotificationSample/NotificationSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotificationSample
+//
+//  Created by 陰山賢太 on 2018/10/26.
+//  Copyright © 2018 Kageken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
